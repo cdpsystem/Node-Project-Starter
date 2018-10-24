f754c4af537bed7693063c6bf1217506d80bb750
Cristian Diaz Prado <cdp.officesystem.com>
2018-10-24 14:29:46 +0200

    Commit
    Creado 404
    Añadido control por IP
    Creado fichero .env.sample

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ let Config = require('./config/config');
 let path = require('path');
 let responseTime = require('response-time');
 
+//Tipo de instancia
+let instance = process.argv[2] || 'dev';
+
 //Middlewares
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -21,31 +24,54 @@ app.use(function (req, res, next) {
     next();
 });
 
+
+//Comrpobación de ip permitida
+let allowedIps = process.env.ALLOWED_IP.split(',');
+console.log(allowedIps)
+
+app.use((req,res,next)=>{
+  let ip = req.ip.split(':').slice(-1)[0];
+  if (allowedIps.indexOf(ip) >= 0){
+    next();
+  }else{
+    return res.status(301).send({message: 'Your IP is not allowed to use this API',ip: ip});  
+  }
+  
+});
+
+//router
+app.use(Config.apiPath,require(Config.routerPath));
+
+//Calculo del tiempo de procesado
 app.use(function(req,res,next){
   req.msStartRequest = Date.now();
   next();
 })
 
-//router
-app.use(Config.apiPath,require(Config.routerPath));
-
-
 //PUG Configuration
 app.set('views', './views')
 app.set('view engine', 'pug')
-
 app.disable('view cache');
 
+//Carpeta estatica
 app.use(express.static(__dirname + '/public'));
 
-app.use(function(req, res, next) {
+
+if(instance === 'dev'){
+  app.use(function(req, res, next) {
     let loadTime = Date.now() - req.msStartRequest;
-   	res.status(404).render('view', 
-   		{
-   			url: req.originalUrl,
-        loadTime: loadTime
-   		}
-   	)
-});
+    let ip = req.ip.split(':').slice(-1)[0];
+    let requestUrl = req.originalUrl;
+    res.status(404).render('view', 
+      {
+        url: req.originalUrl,
+        loadTime: loadTime,
+        ip: ip,
+        error: 404,
+        requestUrl: requestUrl,
+      }
+    )
+  });
+}
 
 module.exports = app;
